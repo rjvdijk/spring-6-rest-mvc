3e9d17843a00ef40eeeaad04b19e1a553a4617d4
Rob van Dijk <rob@optimust.nl>
2024-01-30 12:31:53 +0100

    Implement Customer

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Good Customer")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Average Customer")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Bad Customer")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
+
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id) {
+        log.debug("Get Customer by Id - in service. Id: " + id.toString());
+        return customerMap.get(id);
+    }
+}
