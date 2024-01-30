16cae602d535b1e46de366569021aa9bff274fbe
Rob van Dijk <rob@optimust.nl>
2024-01-30 13:01:39 +0100

    HTTP POST new Beer

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -4,6 +4,8 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +17,14 @@ import java.util.UUID;
 public class BeerController {
 
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @GetMapping
     public List<Beer> listBeers() {
