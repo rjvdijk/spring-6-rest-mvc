f2dc966a0684fa1fc750a6d0b1def4a2b8bb6e09
Rob van Dijk <rob@optimust.nl>
2024-01-30 11:09:48 +0100

    lombok-logging

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -1,11 +1,19 @@
 package guru.springframework.spring6restmvc.controllers;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by id - in controller");
+        return beerService.getBeerById(id);
+    }
 }
