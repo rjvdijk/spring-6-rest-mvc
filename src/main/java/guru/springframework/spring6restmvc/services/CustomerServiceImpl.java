cfb570ab5569a5edb4313e6a42fa1f12fc070fdf
Rob van Dijk <rob@optimust.nl>
2024-01-30 13:25:27 +0100

    HTTP POST new Customer

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -56,4 +56,20 @@ public class CustomerServiceImpl implements CustomerService {
         log.debug("Get Customer by Id - in service. Id: " + id.toString());
         return customerMap.get(id);
     }
+
+    @Override
+    public Customer saveNewCustomer(Customer customer) {
+
+        Customer savedCustomer = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .name(customer.getName())
+                .build();
+
+        customerMap.put(savedCustomer.getId(), savedCustomer);
+
+        return savedCustomer;
+    }
 }
