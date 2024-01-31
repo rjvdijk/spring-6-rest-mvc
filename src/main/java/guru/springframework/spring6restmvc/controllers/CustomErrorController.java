8ef234b74e0535ab00f37c428055f75d28c8116c
Rob van Dijk <rob@optimust.nl>
2024-01-31 23:33:06 +0100

    Custom Validation Errorhandler

---
+++ src/main/java/guru/springframework/spring6restmvc/controllers/CustomErrorController.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6restmvc.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handlerBindErrors(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldErrors());
+    }
+}
