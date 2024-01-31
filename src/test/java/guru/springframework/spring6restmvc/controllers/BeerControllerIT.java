5ddc6a7b5005a325918bd2ea3374e1aacf761c07
Rob van Dijk <rob@optimust.nl>
2024-01-31 12:12:49 +0100

    Implement BeerController integration test

---
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring6restmvc.controllers;
+
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos).hasSize(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testListBeersEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos).isEmpty();
+    }
+}
