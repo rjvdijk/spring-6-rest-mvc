b4664aa7ef42d8c600a50839b986275d6d535f79
Rob van Dijk <rob@optimust.nl>
2024-02-01 10:03:45 +0100

    Database constraint validation

--- src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -24,6 +25,7 @@ public class BeerDTO {
 
     @NotNull
     @NotBlank
+    @Size(max = 255)
     private String upc;
     private Integer quantityOnHand;
 
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,12 +10,26 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 @DataJpaTest
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerNameTooLong() {
+        assertThrowsExactly(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 012345678901234567890123456789012345678901234567890123456789")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("89457575543289")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer() {
