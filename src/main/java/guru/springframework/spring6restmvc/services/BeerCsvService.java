039eaff4b0e302e715c62d309165f241f2a96c06
Rob van Dijk <rob@optimust.nl>
2024-02-01 16:04:40 +0100

    Beer CSV parse-service

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface BeerCsvService {
+
+    List<BeerCSVRecord> convertCSV(File file) throws FileNotFoundException;
+}
