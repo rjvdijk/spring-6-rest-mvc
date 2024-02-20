c751c7a9bc19943d2d3584b88763cd77a5f2bae6
Rob van Dijk <rob@optimust.nl>
2024-02-20 14:46:19 +0100

    363. Spring Security Configuration

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerRestAssuredTest.java
@@ -6,14 +6,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
 public class BeerControllerRestAssuredTest {
 
     @LocalServerPort
     Integer localPort;
+
+    @Configuration
+    public static class TestConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            return http
+                    .authorizeHttpRequests(aut -> aut.anyRequest().permitAll())
+                    .build();
+        }
+    }
 
     @BeforeEach
     void setUp() {
